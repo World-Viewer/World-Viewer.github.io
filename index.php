d1920724c15136378ad95058c96f20621d1faba9
Brelondev <devind0791@gmail.com>
2021-04-12 08:56:10 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 
     <div class="header">
         
-        <img src="wv_logo.png" id="mainLogo" alt="World Viewer Logo">
+        <img src="wv_logo2.png" id="mainLogo" alt="World Viewer Logo">
 
 
 <!--
@@ -82,6 +82,89 @@
         <iframe width="560" height="315" src="https://www.youtube.com/embed/G85dhopGADE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
     </div>
+    
+    
+    <div class='place'>
+<!--35.693503292958304, 139.69949538242176-->
+        <h3>Location: Shinjuku, Tokyo <br> Longitude:139.69949538242176<br>Latitude: 35.693503292958304</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/RQA5RcIZlAM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+       <div class='place'>
+<!--43.0440° N, 87.9140° W-->
+        <h3>Location: Milwaukee, WI <br> Longitude:-87.9140<br>Latitude: 43.0440</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/JATo2lPJcaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+           <div class='place'>
+<!--29.571164382 -95.489498042-->
+        <h3>Location: Houston, Texas <br> Longitude:-95.489498042<br>Latitude: 29.571164382</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/c5_w4yGHmNQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+              <div class='place'>
+
+        <h3>Location: Times Square, NYC <br> Longitude:-73.9847<br>Latitude: 40.7580</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/3XTw2FmCUdE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+               <div class='place'>
+
+        <h3>Location: Las Vegas, NV <br> Longitude:-115.172924<br>Latitude: 36.102578</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/1ZNCtDLUKHw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+     <div class='place'>
+
+        <h3>Location: Las Vegas, NV <br> Longitude:-80.1170<br>Latitude: 26.0087</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cmkAbDUEoyA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+    
+         <div class='place'>
+
+        <h3>Location: Ciudad de México <br> Longitude:-99.1346<br>Latitude: 19.4322</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/WRnHHyKYQsw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+    
+             <div class='place'>
+
+        <h3>Location: Laikipia County, Kenya <br> Longitude: 36.7820<br>Latitude: 0.3606</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/bGpNSsnSlfo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div> 
+    
+    <div class='place'>
+
+        <h3>Location: Place Centrale Verbier, Switzerland <br> Longitude: 7.2265100<br>Latitude: 46.1002000</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/yDKJMdZTEXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+
+    
+    
+
+
+
+
+
+
 
 
 
